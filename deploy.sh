70115adca8f319e3e91efc8ad08fae6a181533ce
zhufengwei <zhufengwei@100tal.com>
2018-12-19 15:01:01 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync -e "ssh -p 61020" -tvzr --exclude-from=.deployignore $PWD/public/*  root@test.listenrain.top:/www/hexo/blog
+rsync -e "ssh -p 61020" -atvzr --delete --exclude-from=.deployignore $PWD/public/ root@test.listenrain.top:/www/hexo/public/
+
